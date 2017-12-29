88592b5f78dfcc06574f99272a1de3e535dd7a8f
zhuyue <kid1412-yuki@sohu.com>
2017-12-29 10:46:42 +0800

    sudoku_counter 0.1

--- src/containers/counter.js
+++ src/containers/counter.js
@@ -49,22 +49,95 @@ class Counter extends Component {
 
     // 循环遍历
     let stack = []
-    for (let col = 0; col < 9; col++) {
+    let onOff = false // 判断是否是再次进入的开关
+    for (let col = 0; col < 9;) {
+      console.log('进入col')
       for (let row = 0; row <9;) {
-        if (allArr[col][row] === 0) {
-          for (let num = 1; num < 10; num ++) {
+        // 如果是返回的值,进入新的逻辑,怎么区分第一次赋值与返回重新赋值?
+        if (stack.length > 81) {
+          console.log('暂停')
+          console.log(stack, 'allArr')
+          console.log(allArr, 'allArr')
+          return
+        }
+        console.log('进入row')
+        if (onOff) {
+          console.log('进入了回溯点')
+          onOff = false
+          if (allArr[col][row] === 9) {
+            // 需要继续回溯
+            allArr[col][row] = 0
+            // 重置col和row
+            console.log('回溯点等于9,再次回溯')
+            console.log(JSON.stringify(stack), 'stack')
+            let obj = stack.pop()
+            console.log(JSON.stringify(stack), 'stack')
+            col = obj.col
+            row = obj.row
+            onOff = true
+          } else {
+            for (let num = allArr[col][row] + 1; num < 10;) {
+              allArr[col][row] = num
+              if (valid(col, row)) {
+                stack.push({
+                  col,
+                  row
+                })
+                console.log(num, '得到了一个正确值, 回溯点内')
+                console.log(onOff, 'onOff')
+                console.log(JSON.stringify(stack), 'stack')
+                break
+              }
+              num ++
+              console.log(num, 'over')
+              if (num === 10) {
+                allArr[col][row] = 0
+                // 重置col和row
+                console.log('没有正确值需要回溯, 回溯点内')
+                console.log(JSON.stringify(stack), 'stack')
+                let obj = stack.pop()
+                console.log(JSON.stringify(stack), 'stack')
+                col = obj.col
+                row = obj.row
+                onOff = true
+                break
+              }
+            }
+          }
+        } else if (allArr[col][row] === 0) {
+          for (let num = 1; num < 10;) {
             allArr[col][row] = num
-            console.log(num)
             if (valid(col, row)) {
-              console.log('break')
+              stack.push({
+                col,
+                row
+              })
+              console.log(num, '得到了一个正确值')
+              console.log(JSON.stringify(stack), 'stack')
               break
+            }
+            num ++
+            console.log(num, 'over')
+            if (num === 10) {
+              allArr[col][row] = 0
+              // 重置col和row
+              console.log(JSON.stringify(stack), 'stack1')
+              let obj = stack.pop()
+              console.log(JSON.stringify(stack), 'stack2')
+              col = obj.col
+              row = obj.row
+              onOff = true
+              console.log(obj.col, obj.row, 'col, row')
+              console.log('没有正确值需要回溯')
             }
           }
         } else {
           row++
         }
       }
+      col++
     }
+    console.log(stack, 'allArr')
     console.log(allArr, 'allArr')
   }
   clear () {
